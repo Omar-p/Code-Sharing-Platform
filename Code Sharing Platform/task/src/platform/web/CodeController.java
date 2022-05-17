e1ae3c2ae8078f10f840ab85fda28cecf70cf99b
Omar Shabaan <omar.plus.077@gmail.com>
2022-05-17 06:40:29 +0200

    add spring-boot-data-jpa and h2 db dependency to start in  stage 3

--- Code Sharing Platform/task/src/platform/web/CodeController.java
+++ Code Sharing Platform/task/src/platform/web/CodeController.java
@@ -3,16 +3,33 @@ package platform.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import platform.services.CodeService;
 
 @Controller
 @RequestMapping("/code")
 public class CodeController {
 
+  private final CodeService codeService;
+
+  public CodeController(CodeService codeService) {
+    this.codeService = codeService;
+  }
+
   @GetMapping
   public String getCode(Model model) {
-    model.addAttribute("code",
-        "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}");
+    model.addAttribute("code", this.codeService.getCode());
     return "code";
+  }
+
+  @GetMapping("new")
+  public String newCode() {
+    return "newCode";
+  }
+
+  @PostMapping("new")
+  public String processNewCode() {
+    return "redirect:/code/new";
   }
 }
--- Code Sharing Platform/task/src/platform/web/CodeResource.java
+++ Code Sharing Platform/task/src/platform/web/CodeResource.java
@@ -1,19 +1,32 @@
 package platform.web;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import platform.model.Code;
+import platform.services.CodeService;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/code")
 public class CodeResource {
+
+  private final CodeService codeService;
+
+  public CodeResource(CodeService codeService) {
+    this.codeService = codeService;
+  }
+
   @GetMapping
   public ResponseEntity<?> getCode() {
-    Map<String, String> body = Map.of("code",
-        "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}");
-    return ResponseEntity.ok(body);
+    return ResponseEntity.ok(this.codeService.getCode());
   }
+
+  @PostMapping("/new")
+  public ResponseEntity<?> submitCode(@RequestBody Code code) {
+    this.codeService.setCode(code);
+    return ResponseEntity.ok("{}");
+  }
+
 }
